7b1abbe81f683866aee8e9558b287bfa6a7b235f
Waleed Lugod <lugodwaleed@gmail.com>
2025-08-30 12:29:31 +0800

    rap

--- Lugod-LongExercise1/Lugod-LongExercise1/Program.cs
+++ Lugod-LongExercise1/Lugod-LongExercise1/Program.cs
@@ -1,7 +1,7 @@
-﻿int totalCost = 0;
-int choice;
+﻿int choice;
 List<Burger> burgers = new List<Burger>();
 List<Side> sides = new List<Side>();
+List<Wrap> wraps = new List<Wrap>();
 
 do
 {
@@ -60,12 +60,10 @@ void OrderBurger()
     hasVeggies = input == "y";
 
     Burger burger = new Burger(extraPatties, extraCheese, hasVeggies);
-    totalCost += burger.cost;
     burgers.Add(burger);
 
-    Console.WriteLine($"Burger with {extraPatties} extra patties, {extraCheese} extra cheese slices added, and {(hasVeggies ? "" : "no")} veggies added! Subtotal: {burger.cost} PHP");
+    Console.WriteLine($"Burger with {extraPatties} extra patties, {(extraCheese > 0 ? extraCheese : "no")} extra cheese slices added, and {(hasVeggies ? "" : "no")} veggies added! Subtotal: {burger.cost} PHP");
 }
-
 void OrderSide()
 {
     string? input;
@@ -83,19 +81,75 @@ void OrderSide()
         input = Console.ReadLine();
     } while (!int.TryParse(input, out type) || type < 1 || type > 3);
 
+    int mediumCost = -1;
+    int largeCost = -1;
+    switch (type)
+    {
+        case 1:
+            mediumCost = 50;
+            largeCost = 75;
+            break;
+        case 2:
+            mediumCost = 60;
+            largeCost = 90;
+            break;
+        case 3:
+            mediumCost = 70;
+            largeCost = 105;
+            break;
+    }
     do
     {
-        Console.Write("What size (m/l)?: ");
+        Console.WriteLine($"""
+        Please select a size:
+        (1) Medium ({mediumCost} PHP)
+        (2) Large ({largeCost} PHP)
+        """);
         input = Console.ReadLine();
-    } while (input != "m" && input != "l");
-    size = input == "m" ? 1 : 2;
+    } while (!int.TryParse(input, out size) || size < 1 || size > 2);
 
     Side side = new Side(type, size);
     sides.Add(side);
 
     Console.WriteLine($"{side.size} {side.type} added! Subtotal: {side.cost} PHP");
 }
-void OrderWrap() { }
+void OrderWrap()
+{
+    string? input;
+    bool isAllMeat;
+    int extraCheese;
+    int spiceLevel;
+
+    do
+    {
+        Console.Write("Would you like your wrap to be all meat? (y/n): ");
+        input = Console.ReadLine();
+    } while (input != "y" && input != "n");
+    isAllMeat = input == "y";
+
+    do
+    {
+        Console.Write("Please enter the number of extra cheese you would like to add (20 PHP per slice): ");
+        input = Console.ReadLine();
+    } while (!int.TryParse(input, out extraCheese) || extraCheese < 0);
+
+    do
+    {
+        Console.WriteLine("""
+        Please select a spice level:
+        (1) Mild
+        (2) Spicy
+        (3) Very Spicy
+        """);
+        input = Console.ReadLine();
+    } while (!int.TryParse(input, out spiceLevel) || spiceLevel < 1 || spiceLevel > 3);
+
+    Wrap wrap = new Wrap(extraCheese, isAllMeat, spiceLevel);
+    wraps.Add(wrap);
+
+    string res = $"{(isAllMeat ? "All meat" : "")} {wrap.spiceLevel?.ToLower()} wrap with {(extraCheese > 0 ? extraCheese : "no")} extra cheese added! Subtotal: {wrap.cost} PHP".Trim();
+    Console.WriteLine($"{char.ToUpper(res[0])}{res[1..]}");
+}
 void ViewItems() { }
 void RemoveItem() { }
 void FinishOrder() { }
@@ -139,5 +193,25 @@ class Side
             case "Onion Rings": cost = this.size == "Medium" ? 60 : 90; break;
             case "Bacon Chips": cost = this.size == "Medium" ? 70 : 105; break;
         }
+    }
+}
+
+class Wrap
+{
+    public int extraCheese;
+    public bool isAllMeat;
+    public string? spiceLevel;
+    public int cost;
+    public Wrap(int extraCheese, bool isAllMeat, int spiceLevel)
+    {
+        this.extraCheese = extraCheese;
+        this.isAllMeat = isAllMeat;
+        switch (spiceLevel)
+        {
+            case 1: this.spiceLevel = "Mild"; break;
+            case 2: this.spiceLevel = "Spicy"; break;
+            case 3: this.spiceLevel = "Very Spicy"; break;
+        }
+        this.cost = 100 + 20 * extraCheese;
     }
 }
